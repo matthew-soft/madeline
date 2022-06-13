23981b2ee334e8ca69466bf5db4fc8e1f4db6215
GNZTMPZ <27568445+mclemie@users.noreply.github.com>
2022-06-14 00:23:12 +0700

    formatted with black

--- extensions/presence.py
+++ extensions/presence.py
@@ -1,5 +1,12 @@
-from naff import (Activity, ActivityType, Extension, IntervalTrigger, Status,
-                  Task, listen)
+from naff import (
+    Activity,
+    ActivityType,
+    Extension,
+    IntervalTrigger,
+    Status,
+    Task,
+    listen,
+)
 from naff.api.events import ChannelCreate
 
 from core.base import CustomClient
--- extensions/tools.py
+++ extensions/tools.py
@@ -7,9 +7,18 @@ import naff
 import requests
 import wget
 from dotenv import load_dotenv
-from naff import (CommandTypes, Embed, Extension, GuildCategory, GuildText,
-                  GuildVoice, OptionTypes, context_menu, slash_command,
-                  slash_option)
+from naff import (
+    CommandTypes,
+    Embed,
+    Extension,
+    GuildCategory,
+    GuildText,
+    GuildVoice,
+    OptionTypes,
+    context_menu,
+    slash_command,
+    slash_option,
+)
 
 load_dotenv()
 
