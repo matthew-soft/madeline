581c1e7501fe6d8f019fb1f8ed68e20005af4221
GNZTMPZ <27568445+mclemie@users.noreply.github.com>
2022-06-20 01:25:41 +0700

    using paginator for `/samp-query` niche view

--- extensions/samp.py
+++ extensions/samp.py
@@ -9,23 +9,13 @@ from naff import (
     slash_command,
     slash_option,
 )
+from naff.ext.paginators import Paginator
 from samp_client.client import SampClient
 
 
 class samp(Extension):
     @slash_command(
-        "samp", description="Show SA-MP server info and basic player information"
-    )
-    @slash_option(
-        name="type",
-        description="Type of information to show",
-        required=True,
-        opt_type=OptionTypes.INTEGER,
-        choices=[
-            SlashCommandChoice(name="General vibes", value=1),
-            SlashCommandChoice(name="Server info only", value=2),
-            SlashCommandChoice(name="Player info only", value=3),
-        ],
+        "samp-query", description="Show SA-MP server info and basic player information"
     )
     @slash_option(
         "ip",
@@ -47,108 +37,115 @@ class samp(Extension):
         # need to defer it, otherwise, it fails
         await ctx.defer()
         try:
-            if type == 1:
-                embed = Embed(title=info.hostname, color=0x0083F5)  # Create embed
-                embed.add_field(name="IP", value=f"`{ip}:{port}`", inline=True)
-                embed.add_field(
-                    name="Gamemode : ", value=f"`{info.gamemode}`", inline=True
+            general = Embed(title=info.hostname, color=0x0083F5)  # Create embed
+            general.add_field(name="IP", value=f"`{ip}:{port}`", inline=True)
+            general.add_field(
+                name="Gamemode : ", value=f"`{info.gamemode}`", inline=True
+            )
+            general.add_field(
+                name="Language : ", value=f"`{info.language}`", inline=True
+            )
+            general.add_field(
+                name="Passworded? : ", value=f"`{info.password}`", inline=True
+            )
+            general.add_field(
+                name="Players : ",
+                value=f"`{info.players}` / `{info.max_players}` Players",
+                inline=True,
+            )
+            if info.players == 0:
+                general.add_field(
+                    name="[only show 10 player max] Connected Clients :",
+                    value="No players connected",
+                    inline=False,
                 )
-                embed.add_field(
-                    name="Language : ", value=f"`{info.language}`", inline=True
-                )
-                embed.add_field(
-                    name="Passworded? : ", value=f"`{info.password}`", inline=True
-                )
-                embed.add_field(
-                    name="Players : ",
-                    value=f"`{info.players}` / `{info.max_players}` Players",
-                    inline=True,
-                )
-                if info.players == 0:
-                    embed.add_field(
+            if info.players > 0:
+                for ppq in numpang:
+                    general.add_field(
                         name="[only show 10 player max] Connected Clients :",
-                        value="No players connected",
+                        value=f"```==============================================\nName                        | Score\n ==============================================\n {ppq.name}                    | {ppq.score}```",
                         inline=False,
                     )
-                if info.players > 0:
-                    for ppq in numpang:
-                        embed.add_field(
-                            name="[only show 10 player max] Connected Clients :",
-                            value=f"```==============================================\nName                        | Score\n ==============================================\n {ppq.name}                    | {ppq.score}```",
+            if info.players > 10:
+                general.add_field(
+                    name="Note:",
+                    value="due to __*discord limitations*__, i can't show connected clients summary 😔",
+                    inline=False,
+                )
+            general.set_footer(
+                text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url
+            )
+            general.timestamp = datetime.datetime.utcnow()
+
+            srv_info = Embed(title=info.hostname, color=0x0083F5)  # Create embed
+            srv_info.add_field(name="IP", value=f"`{ip}:{port}`", inline=True)
+            srv_info.add_field(
+                name="Gamemode : ", value=f"`{info.gamemode}`", inline=True
+            )
+            srv_info.add_field(
+                name="Language : ", value=f"`{info.language}`", inline=True
+            )
+            srv_info.add_field(
+                name="Passworded? : ", value=f"`{info.password}`", inline=True
+            )
+            srv_info.set_footer(
+                text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url
+            )
+            srv_info.timestamp = datetime.datetime.utcnow()
+
+            if info.players == 0:
+                p_info = Embed(
+                    description=f"<:cross:839158779815657512> No players connected",
+                    color=0xFF0000,
+                )  # Create embed
+                p_info.set_footer(
+                    text=f"Requested by {ctx.author}",
+                    icon_url=ctx.author.avatar.url,
+                )
+            else:
+                if info.players > 10:
+                    p_info = Embed(
+                        title=info.hostname,
+                        description="Note: due to __*discord limitations*__, i can't show detailed connected clients 😔",
+                        color=0xF6ADAB,
+                    )  # Create embed
+                    p_info.add_field(
+                        name="Players : ",
+                        value=f"`{info.players}` / `{info.max_players}` Players",
+                        inline=True,
+                    )
+                    p_info.set_footer(
+                        text=f"Requested by {ctx.author}",
+                        icon_url=ctx.author.avatar.url,
+                    )
+                    p_info.timestamp = datetime.datetime.utcnow()
+                else:
+                    p_info = Embed(title=info.hostname, color=0x4C8404)  # Create embed
+                    p_info.add_field(
+                        name="Players : ",
+                        value=f"`{info.players}` / `{info.max_players}` Players",
+                        inline=True,
+                    )
+                    for ppq in players:
+                        p_info.add_field(
+                            name="[only show 10 player max] Detailed Connected Clients :",
+                            value=f"```==============================================\nID |Name                        | Score | Ping\n ==============================================\n {ppq.id} | {ppq.name}                    | {ppq.score} | {ppq.ping}```",
                             inline=False,
                         )
-                if info.players > 10:
-                    embed.add_field(
-                        name="Note:",
-                        value="due to __*discord limitations*__, i can't show connected clients summary 😔",
-                        inline=False,
+                    p_info.set_footer(
+                        text=f"Requested by {ctx.author}",
+                        icon_url=ctx.author.avatar.url,
                     )
-                embed.set_footer(
-                    text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url
-                )
-                embed.timestamp = datetime.datetime.utcnow()
-                await ctx.send(embed=embed)  # Send the embed
-            if type == 2:
-                embed = Embed(title=info.hostname, color=0x0083F5)  # Create embed
-                embed.add_field(name="IP", value=f"`{ip}:{port}`", inline=True)
-                embed.add_field(
-                    name="Gamemode : ", value=f"`{info.gamemode}`", inline=True
-                )
-                embed.add_field(
-                    name="Language : ", value=f"`{info.language}`", inline=True
-                )
-                embed.add_field(
-                    name="Passworded? : ", value=f"`{info.password}`", inline=True
-                )
-                embed.set_footer(
-                    text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url
-                )
-                embed.timestamp = datetime.datetime.utcnow()
-                await ctx.send(embed=embed)  # Send the embed
-            if type == 3:
-                if info.players == 0:
-                    embed = Embed(
-                        description=f"<:cross:839158779815657512> No players connected",
-                        color=0xFF0000,
-                    )  # Create embed
-                else:
-                    if info.players > 10:
-                        embed = Embed(
-                            title=info.hostname,
-                            description="Note: due to __*discord limitations*__, i can't show detailed connected clients 😔",
-                            color=0xF6ADAB,
-                        )  # Create embed
-                        embed.add_field(
-                            name="Players : ",
-                            value=f"`{info.players}` / `{info.max_players}` Players",
-                            inline=True,
-                        )
-                        embed.set_footer(
-                            text=f"Requested by {ctx.author}",
-                            icon_url=ctx.author.avatar.url,
-                        )
-                        embed.timestamp = datetime.datetime.utcnow()
-                    else:
-                        embed = Embed(
-                            title=info.hostname, color=0x4C8404
-                        )  # Create embed
-                        embed.add_field(
-                            name="Players : ",
-                            value=f"`{info.players}` / `{info.max_players}` Players",
-                            inline=True,
-                        )
-                        for ppq in players:
-                            embed.add_field(
-                                name="[only show 10 player max] Detailed Connected Clients :",
-                                value=f"```==============================================\nID |Name                        | Score | Ping\n ==============================================\n {ppq.id} | {ppq.name}                    | {ppq.score} | {ppq.ping}```",
-                                inline=False,
-                            )
-                        embed.set_footer(
-                            text=f"Requested by {ctx.author}",
-                            icon_url=ctx.author.avatar.url,
-                        )
-                        embed.timestamp = datetime.datetime.utcnow()
-                await ctx.send(embed=embed)  # Send the embed
+                    p_info.timestamp = datetime.datetime.utcnow()
+            embeds = [general, srv_info, p_info]
+
+            paginators = Paginator(
+                client=self.bot,
+                pages=embeds,
+                timeout_interval=30,
+                show_select_menu=False,
+            )
+            await paginators.send(ctx)
         except:
             embed = Embed(
                 description=f"<:cross:839158779815657512> Couldn't connect to the server",
