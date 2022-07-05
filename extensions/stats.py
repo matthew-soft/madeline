0beabac3a445657064ea6cd94104ed678c6f6885
GNZTMPZ <27568445+clemiee@users.noreply.github.com>
2022-07-06 02:57:50 +0700

    minor typofix

--- extensions/stats.py
+++ extensions/stats.py
@@ -24,7 +24,7 @@ class stats(Extension):
 
     def get_bot_uptime(self, *, brief=False):
         now = datetime.datetime.utcnow()
-        delta = now - self.bot.uptime
+        delta = now - self.bot.start_time
         hours, remainder = divmod(int(delta.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
         days, hours = divmod(hours, 24)
