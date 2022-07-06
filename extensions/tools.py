fa582b97a8b540c091e99d021c1236fb31ffe0ee
GNZTMPZ <27568445+clemiee@users.noreply.github.com>
2022-07-06 12:48:33 +0700

    merge ddocs to tools ext

--- extensions/tools.py
+++ extensions/tools.py
@@ -8,6 +8,7 @@ import naff
 import requests
 import wget
 from dotenv import load_dotenv
+from algoliasearch.search_client import SearchClient
 from naff import (
     CommandTypes,
     Embed,
@@ -28,6 +29,11 @@ load_dotenv()
 class tools(Extension):
     def __init__(self, bot):
         self.bot = bot
+        ## Fill out from trying a search on the ddevs portal
+        app_id = "BH4D9OD16A"
+        api_key = "f37d91bd900bbb124c8210cca9efcc01"
+        self.search_client = SearchClient.create(app_id, api_key)
+        self.index = self.search_client.init_index("discord")
 
     @context_menu("Guild Avatar", CommandTypes.USER)
     async def context_guild_avatar(self, ctx):
@@ -322,6 +328,50 @@ class tools(Extension):
             await ctx.send(file=pepek)
             # purge the cache
             os.remove(path=pepek)
+    
+    @slash_command(
+        "ddocs", description="Scours the discord api documentations for help"
+    )
+    @slash_option(
+        name="search_term",
+        description="Name of the plugin to get the commands for",
+        required=True,
+        opt_type=OptionTypes.STRING,
+    )
+    async def ddocs(self, ctx, *, search_term):
+
+        results = await self.index.search_async(search_term)
+        description = ""
+        hits = []
+        for hit in results["hits"]:
+            title = self.get_level_str(hit["hierarchy"])
+            if title in hits:
+                continue
+            hits.append(title)
+            url = hit["url"].replace(
+                "https://discord.com/developers/docs", "https://discord.dev"
+            )
+            description += f"[{title}]({url})\n"
+            if len(hits) == 10:
+                break
+        embed = Embed(
+            title="Your help has arrived!",
+            description=description,
+            color=0x7289DA,
+        )
+        embed.set_footer(
+            text=f"Requested by {ctx.author} | Powered by Algolia DocSearch",
+            icon_url=ctx.author.avatar.url,
+        )
+        embed.timestamp = datetime.datetime.utcnow()
+        return await ctx.send(embed=embed)
+
+    def get_level_str(self, levels):
+        last = ""
+        for level in levels.values():
+            if level is not None:
+                last = level
+        return last
 
 
 def setup(bot):
