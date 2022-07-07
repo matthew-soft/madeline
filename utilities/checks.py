127814df74fda0371bb451a1ba4caa37a2f191f4
GNZTMPZ <85807297+GNZTMPZ@users.noreply.github.com>
2022-07-07 23:33:06 +0700

    remove unused checks

--- utilities/checks.py
+++ utilities/checks.py
@@ -3,7 +3,6 @@ from typing import Awaitable, Callable, Union
 
 from naff import Permissions
 from naff.client.errors import *
-from naff.models.discord.snowflake import Snowflake_Type, to_snowflake
 from naff.models.naff.context import Context
 
 TYPE_CHECK_FUNCTION = Callable[[Context], Awaitable[bool]]
@@ -11,25 +10,6 @@ TYPE_CHECK_FUNCTION = Callable[[Context], Awaitable[bool]]
 
 class MissingPermissions(CommandException):
     """User is missing permissions"""
-
-
-class RoleNotFound(CommandException):
-    """Role was not found in the guild"""
-
-
-class UserNotFound(CommandException):
-    """User was not found in the guild"""
-
-
-class MissingRole(CommandException):
-    """Member is missing a role"""
-
-
-async def has_perms(author, perm):
-    adminrole = [role for role in author.roles if perm in role.permissions]
-    if adminrole != []:
-        return True
-
 
 def member_permissions(*permissions: Permissions) -> TYPE_CHECK_FUNCTION:
     """
@@ -44,18 +24,5 @@ def member_permissions(*permissions: Permissions) -> TYPE_CHECK_FUNCTION:
             return False
         if any(ctx.author.has_permission(p) for p in permissions):
             return True
-
-    return check
-
-
-def is_owner():
-    """
-    Is the author the owner of the bot.
-    parameters:
-        coro: the function to check
-    """
-
-    async def check(ctx: Context) -> bool:
-        return ctx.author.id == 351150966948757504
 
     return check
