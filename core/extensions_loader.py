2cb15a3e40f67a0eb9e4066500203f888106faee
Clemiee McCartney <git@gnztmpz.eu.org>
2022-10-11 00:50:18 +0700

    `cogs` instead `extensions`

--- core/extensions_loader.py
+++ core/extensions_loader.py
@@ -4,13 +4,13 @@ from core.base import CustomClient
 
 
 def load_extensions(bot: CustomClient):
-    """Automatically load all extension in the ./extensions folder"""
+    """Automatically load all extension in the ./cogs folder"""
 
     bot.logger.info("Loading Extensions...")
 
     # go through all folders in the directory and load the extensions from all files
     # Note: files must end in .py
-    for root, dirs, files in os.walk("extensions"):
+    for root, dirs, files in os.walk("cogs"):
         for file in files:
             if file.endswith(".py") and not file.startswith("__init__"):
                 file = file.removesuffix(".py")
