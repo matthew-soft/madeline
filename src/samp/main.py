79ad9569530bc5c3512cdef99571af60bd501fce
Clemiee McCartney <git@gnztmpz.eu.org>
2022-10-22 12:58:57 +0700

    samp player table init

--- src/samp/main.py
+++ src/samp/main.py
@@ -3,6 +3,7 @@ import datetime
 import cloudscraper
 from naff import Embed
 from samp_client.client import SampClient
+from markdownTable import markdownTable
 
 scraper = cloudscraper.create_scraper()
 
